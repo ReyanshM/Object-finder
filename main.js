56f0f997a2c0bd11ff502e87e1da7d5b93091be5
ReyanshM <72616867+ReyanshM@users.noreply.github.com>
2021-08-10 18:36:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,42 +6,39 @@ function setup(){
     video=createCapture(VIDEO);
     video.size(600,500);
     video.hide();
-    // objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
 }
 function modelLoaded(){
     console.log("The model is loaded");
-    staus="true";
 }
 function Results(error, results){
     if(error){
         console.log(error);
     }
-    // else{
-    //     console.log(results);
-    //     res=results;
-    //     resLength=results.length;
-    // }
+    else{
+        console.log(results);
+        res=results;
+        resLength=results.length;
+    }
 }
 function search(){
     if(document.getElementById('searchRegister').value!=""){
-        console.log(document.getElementById('searchRegister').value);
+        searchId=document.getElementById('searchRegister').value;
+        status="true";
     }
 }
 function draw(){
     image(video,0,0,600,500);
-    // objectDetector.detect(video, Results);
+    objectDetector.detect(video, Results);
     console.log(document.getElementById('searchRegister').value);
-    // for(m=0;m<resLength;m++){
-    //     if(status=="false"){
-    //         document.getElementById('status').textContent="Searching for " + document.getElementById('searchRegister').value;
-    //     }
-    //     else if(status=="true"){
-    //         if(res[m].label==document.getElementById('searchRegister').value){
-    //             document.getElementById('status').textContent=document.getElementById('searchRegister').value + " found";       
-    //         }
-    //         else{
-    //             document.getElementById('status').textContent="Searching for " + document.getElementById('searchRegister').value;
-    //         }
-    //     }
-    // }
+    if(status=="true"){
+        for(m=0;m<resLength;m++){
+            if(res[m].label==searchId.toLowerCase()){
+                document.getElementById('status').textContent=searchId + " is found";
+            }
+            else{
+                document.getElementById('status').textContent=searchId + " is not found";
+            }
+        }
+    }
 }
